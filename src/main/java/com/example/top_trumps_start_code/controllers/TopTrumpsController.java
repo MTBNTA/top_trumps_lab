11e8e71b141bdbb8ab5e44f70e23bfe71e1ee2b0
Maya T <tettehmaya5@gmail.com>
2024-02-26 16:09:01 +0000

    Removed unused post mapping in controller

--- src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
+++ src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
@@ -1,8 +1,6 @@
 package com.example.top_trumps_start_code.controllers;
 
 import com.example.top_trumps_start_code.models.Card;
-import com.example.top_trumps_start_code.models.Rank;
-import com.example.top_trumps_start_code.models.Suit;
 import com.example.top_trumps_start_code.services.TopTrumpsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,12 +21,5 @@ public class TopTrumpsController {
       String card = topTrumpsService.checkWinner(cards);
       return new ResponseEntity<>(card, HttpStatus.OK);
   }
-
-//      @PostMapping
-//    public ResponseEntity<> newGame(){
-//        String card = topTrumpsService.checkWinner();
-//        return new ResponseEntity<>(cards, HttpStatus.CREATED);
-//  }
-
 
 }
